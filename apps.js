0d4d791ea72f3eb89d5ddf88bdfc57b5859b4d0a
unknown <gabsslemiabaya@gmail.com>
2024-09-21 13:17:34 +0800

    Completed Variable Section

---
+++ apps.js
@@ -0,0 +1,9 @@
+let x = " si angela ";
+let xlength= x.length;
+let changeCaps= x.toUpperCase();
+let changeLows= x.toLowerCase();
+
+console.log(x);
+console.log(xlength);
+console.log(changeCaps);
+console.log(changeLows);
---
+++ variable.js
@@ -0,0 +1,8 @@
+let bossG = "Xyril Gabriel L. Abaya";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Male";
+let fbHandle = "Xyril Gabriel Abaya";
+console.log(bossG);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(fbHandle);
